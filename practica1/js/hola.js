1a2f606822734568d882f7fb2e2f90d4f0fc9d54
Greys2004 <gav77465@lasallebajio.edu.mx>
2024-08-19 09:12:40 -0600

    Practica 1

---
+++ practica1/js/hola.js
@@ -0,0 +1,5 @@
+var mensaje = 'Hola TS';
+console.log(mensaje);
+var numero = 10;
+numero = 20;
+console.log(numero);
---
+++ practica1/src/hola.ts
@@ -0,0 +1,7 @@
+let mensaje: string = 'Hola TS';
+console.log(mensaje);
+
+let numero: number = 10;
+numero = 20;
+console.log(numero);
+
