b66f34bb0b5c937f937610204c1d3ad125994e1d
Greys2004 <gav77465@lasallebajio.edu.mx>
2024-08-20 08:40:25 -0600

    Practica 2 Final

--- practica2/src/index.ts
+++ practica2/src/index.ts
@@ -10,3 +10,20 @@ console.log(booleano);
 let encendido: boolean;
 encendido = true;
 console.log(encendido);
+
+console.log('Encendido es:', encendido);
+console.log('Encendido es:', {encendido});
+
+encendido = true && false;
+encendido = true && true;
+encendido = false && true;
+encendido = false && false;
+console.log('Resultado de encendido:',encendido);
+
+let normal:any = 'hola';
+normal = 10;
+normal = true;
+normal = 11.11;
+console.log('Normal:',normal);
+
+
