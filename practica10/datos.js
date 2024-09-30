2b8e46894fe2f1d3c3e2481024653e2f4177c8e2
Greys2004 <gav77465@lasallebajio.edu.mx>
2024-09-30 09:35:46 -0600

    practica 11 y 12

---
+++ practica10/datos.js
@@ -0,0 +1,5 @@
+let nombre = 'Greys';
+let apellido = 'Almanza';
+let edad = 20;
+
+module.exports = {nombre, apellido, edad};
---
+++ practica10/index.js
@@ -0,0 +1,5 @@
+const {nombre,apellido,edad} = require ('./datos');
+const obtenerDatos = require('./obtenerDatos.js')
+
+console.log(obtenerDatos(nombre,apellido,edad))
+
