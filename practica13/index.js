2a252c15b79e5241faa7abf6b72b3ba1c549e6bc
Greys2004 <gav77465@lasallebajio.edu.mx>
2024-10-07 08:06:20 -0600

    practica12

---
+++ practica13/index.js
@@ -0,0 +1,22 @@
+//npm init
+//npm install express
+const express = require('express');
+
+// constante que contenga express
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola Greys');
+});
+
+app.get('/productos', (req, res) => {
+    res.send('Estamos en productos');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en puerto 3000');
+});
+
+//nodemon es una herramienta que ayuda a desarrollar aplicaciones basadas en node.js reiniciando automáticamente la aplicación de node cuando se detectan cambios de archivo en el directorio.
+//Sirve para no estar poniendo continuamente "node index.js"
+//nodemon install nodemon
